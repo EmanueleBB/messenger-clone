db1e5efaf3983e73b5d7338deedb024da1ba72fc
EmanueleBB <121090087+EmanueleBB@users.noreply.github.com>
2023-09-13 10:50:21 +0200

    completed NextAuth setup

--- app/api/auth/[...nextAuth]/route.ts
+++ app/api/auth/[...nextAuth]/route.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import NextAuth, {AuthOptions} from "next-auth";
-import { CredentialsProvider } from "next-auth/providers";
+import  CredentialsProvider  from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -27,6 +27,57 @@ export const authOptions : AuthOptions = {
       GithubProvider({
          clientId:process.env.GITHUB_ID as string,
          clientSecret:process.env.GITHUB_SECRET as string,
+      }),
+      GoogleProvider({
+         clientId:process.env.GOOGLE_CLIENT_ID as string,
+         clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+      CredentialsProvider({
+         name:'credentials',
+         credentials:{
+            email:{ label:'email', type:'text'},
+            password:{ label:'password', type:'password'},
+         },
+         async authorize(credentials){
+            if(!credentials?.email || !credentials?.password){
+               throw new Error('Invalid Credentials');
+            }
+
+            const user = await prisma.user.findUnique({
+               where:{
+                  email:credentials.email
+               }
+            });
+
+            //This happens if the user doesn't exist or if he signed up with Google or Github (so he doesn't have an explicit password to type out)
+            if(!user || !user?.hashedPassword){
+               throw new Error('Invalid credentials')
+            }
+
+
+            //in the database we have an hashedPassword, that we still have to create through the form. The await is needed in order to make isCorrectPassword a boolean an not a Promise<boolean>
+            const isCorrectPassword = await bcrypt.compare(
+               credentials.password,
+               user.hashedPassword
+            );
+
+            if(!isCorrectPassword){
+               throw new Error('Invalid credentials')
+            }
+
+            return user;
+         }
       })
-   ]
-}
+   ],
+
+   debug: process.env.NODE_ENV === 'development',
+   session:{
+      strategy:"jwt"
+   },
+   secret:process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+//This is a new Next JS requirement
+export {handler as GET, handler as POST}
