07bd0d65711e8dd357cdb3b8b0b402109ef3ba4c
EmanueleBB <121090087+EmanueleBB@users.noreply.github.com>
2023-10-17 10:21:20 +0200

    worked on conversations page

--- app/actions/getConversations.ts
+++ app/actions/getConversations.ts
@@ -12,10 +12,28 @@ const getConversations = async () => {
       const conversations = await prisma.conversation.findMany({
          orderBy:{
             lastMessageAt:'desc'
+         },
+         where:{
+            userIds:{
+               has:currentUser.id
+            }
+         },
+         include:{
+            users:true,
+            messages:{
+               include:{
+                  sender:true,
+                  seen:true //an array of people who saw the last message
+               }
+            },
+
          }
-      })
+      });
+
+      return conversations;
    }catch(error:any){
       return [];
    }
-
 }
+
+export default getConversations;
